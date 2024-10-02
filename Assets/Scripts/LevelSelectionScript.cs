f2cd71bd053c4704c78792a4dd9df445428febc6
haruna38 <nxhoa96@gmail.com>
2024-10-03 01:19:32 +0900

    Make Lv1 terrain a prefab (future use)

---
+++ Assets/Scripts/LevelSelectionScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectionScript : MonoBehaviour
+{
+    public GameObject gameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Debug.Log(Camera.allCameras.length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
