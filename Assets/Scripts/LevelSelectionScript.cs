54b76b5276f52c70323dd0800ce29483b86d7b58
haruna38 <nxhoa96@gmail.com>
2024-10-03 03:48:56 +0900

    Basic selection screen concept

--- Assets/Scripts/LevelSelectionScript.cs
+++ Assets/Scripts/LevelSelectionScript.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class LevelSelectionScript : MonoBehaviour
 {
-    public GameObject gameObject;
     // Start is called before the first frame update
     void Start()
     {
