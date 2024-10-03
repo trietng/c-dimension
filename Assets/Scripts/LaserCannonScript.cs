190cd9dbc9409b652b413e05162b261a9606f628
trietng <93473943+trietng@users.noreply.github.com>
2024-10-03 13:03:58 +0700

    Add laser cannon (working)

--- Assets/Scripts/LaserCannonScript.cs
+++ Assets/Scripts/LaserCannonScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,48 +6,62 @@ using UnityEngine;
 
 public class LaserCannonScript : MonoBehaviour
 {
-    public static float MAX_DISTANCE = 1000f;
+    private static readonly float MAX_DISTANCE = 1000f;
+    private readonly RaycastHit[] hits = new RaycastHit[32];
     public Transform muzzle;
     public GameObject shotPrefab;
-    private VolumetricLines.VolumetricLineStripBehavior lineStrip;
+    private GameObject shot;
+    private VolumetricLines.VolumetricLineBehavior lineBehavior;
+    private BoxCollider shotCollider;
 
     void Start()
     {
-        lineStrip = shotPrefab.GetComponent<VolumetricLines.VolumetricLineStripBehavior>();
+        shot = Instantiate(shotPrefab, muzzle.position, transform.rotation);
+        lineBehavior = shot.GetComponent<VolumetricLines.VolumetricLineBehavior>();
+        shotCollider = shot.GetComponent<BoxCollider>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.T))
-		{
-			GameObject go = Instantiate(shotPrefab, muzzle.position, muzzle.rotation);
-			Destroy(go, 30f);
-		}
+        Fire();
     }
 
-    void Fire(Vector3 origin, Vector3 dir, int reflectCount)
+    public void Fire()
     {
-        var lineVertices = lineStrip.LineVertices;
-        // lineRenderer.positionCount = reflectCount + 1;
-        // Set the start position of the laser
-        for (int i = 0; i < reflectCount; ++i) {
-            // Ignore the Confiner object
-            RaycastHit[] hits = new RaycastHit[100];
-            Physics.RaycastNonAlloc(origin, dir, hits, MAX_DISTANCE);
-            if (hits.Length == 0) {
-                continue;
-            }
-            RaycastHit destination = hits.Last();
-            for (int j = 0; j < hits.Length - 1; j++)
-            {
-                // Do something here
-            }
-            // lineRenderer.SetPosition(i + 1, destination.point);
-            // Instantiate a light spot at the destination point if it doesn't already exist
-            // TODO: Implement this
-            origin = new Vector3(destination.point.x, destination.point.y, destination.point.z);
-            dir = Vector3.Reflect(dir, destination.normal);
+        // Last hit is the closest hit
+        int hitCount = Physics.RaycastNonAlloc(muzzle.position, -muzzle.forward, hits, MAX_DISTANCE);
+        if (hits.Length == 0) {
+            return;
         }
+        Vector3 destination = hits.First().point;
+        for (int j = hitCount - 1; j > 0; j--)
+        {
+            // TODO: Kill player if hit
+
+            // If the hit target can block the laser, then the laser should stop at the hit point.
+            if (hits[j].collider.gameObject.CompareTag("BlockLaser"))
+            {
+                destination = hits[j].point;
+                break;
+            }
+        }
+        Debug.DrawLine(muzzle.position, destination, Color.red, 1f);
+        lineBehavior.StartPos = muzzle.localPosition;
+        float distance = Vector3.Distance(muzzle.position, destination) / shot.transform.localScale.z;
+        lineBehavior.EndPos = new Vector3(
+            muzzle.localPosition.x,
+            muzzle.localPosition.y,
+            muzzle.localPosition.z - distance
+        );
+        shotCollider.center = new Vector3(
+            shotCollider.center.x,
+            shotCollider.center.y,
+            - distance / 2
+        );
+        shotCollider.size = new Vector3(
+            shotCollider.size.x,
+            shotCollider.size.y,
+            - distance
+        );
     }
 }
