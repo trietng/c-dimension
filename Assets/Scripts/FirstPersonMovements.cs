96cf27e4600de9a3a7d0ae0378b62e86950bb815
hikki0901 <95176051+hikki0901@users.noreply.github.com>
2024-10-03 19:45:30 +0700

    add jump + respawn for 1st person

--- Assets/Scripts/FirstPersonMovements.cs
+++ Assets/Scripts/FirstPersonMovements.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class FirstPersonMovements : MonoBehaviour
@@ -7,11 +8,22 @@ public class FirstPersonMovements : MonoBehaviour
     [Header("Movement")]
     public float moveSpeed;
 
+    [Header("Jump")]
+    public bool enableJump;
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
+
     [Header("Ground Check")]
     public float playerHeight;
     public LayerMask ground;
     bool grounded;
     public float groundDrag;
+
+    [Header("Respawn Settings")]
+    public Vector3 respawnPosition;
+    public Quaternion respawnRotation;
 
     public Transform orientation;
 
@@ -26,16 +38,20 @@ public class FirstPersonMovements : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+
+        readyToJump = true;
     }
 
     void Update()
     {
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, ground);
+        
         if (grounded) rb.drag = groundDrag;
         else rb.drag = 0;
         
         Inputs();
         speedControl();
+        Respawn();
     }
 
     private void FixedUpdate()
@@ -47,13 +63,26 @@ public class FirstPersonMovements : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (enableJump)
+        {
+            if (Input.GetKey(KeyCode.Space) && readyToJump && grounded)
+            {
+                readyToJump = false;
+
+                Jump();
+
+                Invoke(nameof(ResetJump), jumpCooldown);
+            }
+        }
     }
 
     private void Movements()
     {
         moveDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        rb.AddForce(moveDir.normalized * moveSpeed * 10f, ForceMode.Force);
+        if (grounded) rb.AddForce(moveDir.normalized * moveSpeed * 10f, ForceMode.Force);
+        else rb.AddForce(moveDir.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
     }
 
     private void speedControl()
@@ -64,6 +93,29 @@ public class FirstPersonMovements : MonoBehaviour
         {
             Vector3 limitedVelocity = flatVelocity.normalized * moveSpeed;
             rb.velocity = new Vector3(limitedVelocity.x, rb.velocity.y, limitedVelocity.z);
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+
+    private void Respawn()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.position = respawnPosition;
+            transform.rotation = respawnRotation;
+
+            rb.velocity = Vector3.zero;
         }
     }
 }
