e65183ea0274f4627e41dbf8bffe3e54d64c5558
minhvip08 <minhvip08@gmail.com>
2024-10-05 14:32:52 +0700

    add Audio Manager

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Audio Sources")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("Audio Clips")]
+    AudioClip backgroundMusic;
+
+}
