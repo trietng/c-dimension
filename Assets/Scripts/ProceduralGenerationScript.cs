362d059873ef39c555af23c23944d4b856d9bb89
trietng <93473943+trietng@users.noreply.github.com>
2024-10-05 12:14:21 +0700

    update level 06 goal and some proc gen tweaks

--- Assets/Scripts/ProceduralGenerationScript.cs
+++ Assets/Scripts/ProceduralGenerationScript.cs
@@ -16,13 +16,11 @@ public class ProceduralGenerationScript : MonoBehaviour
     }
 
     public GameObject[] blockPrefabs;
-
     public GameObject[] surfaceBlockPrefabs;
-
     public GameObject[] treePrefabs;
-
     public GameObject[] palmPrefabs;
-
+    public GameObject bedrockPrefab;
+    public GameObject goalPrefab;
     public int HEIGHT = 64;
     public int WIDTH = 64;
     public int DEPTH = 16;
@@ -32,39 +30,62 @@ public class ProceduralGenerationScript : MonoBehaviour
 
     // Spawn point for player that avoids water
     private Vector3 playerSpawnPoint;
+    private readonly HashSet<Vector2> blacklist = new();
+    private const int playerGoalMinimumDistance = 8;
 
     void Start()
     {
         evalution = new float[WIDTH, HEIGHT];
         Generate();
-        LocatePlayerSpawnPoint();
         Draw();
         GrowPoissonForest();
+        LocatePlayerSpawnPoint();
+        StartCoroutine(GenerateGoal());
     }
 
     void Draw()
     {
         // Inner blocks
-        for (int x = 0; x < WIDTH; x++)
+        for (int x = 1; x < WIDTH - 1; x++)
         {
-            for (int y = 0; y < HEIGHT; y++)
+            for (int y = 1; y < HEIGHT - 1; y++)
             {
                 int height = Height(evalution[x, y]);
                 BlockType type = Biome(evalution[x, y]);
                 if (type == BlockType.WATER) {
                     StartCoroutine(WaterColumn(x, y, type));
+                    blacklist.Add(new Vector2(x, y));
                 }
                 else {
                     StartCoroutine(Column(x, y, height, type));
                 }
             }
         }
+        // Outer blocks
+        for (int x = 0; x < WIDTH; x++)
+        {
+            int height = Height(evalution[x, 0]);
+            BlockType type = Biome(evalution[x, 0]);
+            StartCoroutine(BorderColumn(x, 0, height, type));
+            height = Height(evalution[x, HEIGHT - 1]);
+            type = Biome(evalution[x, HEIGHT - 1]);
+            StartCoroutine(BorderColumn(x, HEIGHT - 1, height, type));
+        }
+        for (int y = 1; y < HEIGHT - 1; y++)
+        {
+            int height = Height(evalution[0, y]);
+            BlockType type = Biome(evalution[0, y]);
+            StartCoroutine(BorderColumn(0, y, height, type));
+            height = Height(evalution[WIDTH - 1, y]);
+            type = Biome(evalution[WIDTH - 1, y]);
+            StartCoroutine(BorderColumn(WIDTH - 1, y, height, type));
+        }
     }
 
     IEnumerator WaterColumn(int x, int y, BlockType type)
     {
-        Instantiate(blockPrefabs[(int)type], new Vector3(x, 0, y), Quaternion.identity);
         Instantiate(surfaceBlockPrefabs[(int)type], new Vector3(x, 2, y), Quaternion.identity);
+        Instantiate(blockPrefabs[(int)type], new Vector3(x, 0, y), Quaternion.identity);
         yield return null;
     }
 
@@ -73,7 +94,7 @@ public class ProceduralGenerationScript : MonoBehaviour
         Instantiate(surfaceBlockPrefabs[(int)type], new Vector3(x, height - 1, y), Quaternion.identity);
         yield return null;
         GameObject block = blockPrefabs[(int)type];
-        int d = HeightDifference(x, y);
+        int d = MaxHeightDifference(x, y);
         for (int z = height - d; z < height - 1; z++)
         {
             Instantiate(block, new Vector3(x, z, y), Quaternion.identity);
@@ -81,16 +102,38 @@ public class ProceduralGenerationScript : MonoBehaviour
         }
     }
 
-    int HeightDifference(int x, int y)
+    IEnumerator BorderColumn(int x, int y, int height, BlockType type)
     {
-        int dLeft = x > 0 ? HeightDifferenceRaw(evalution[x, y], evalution[x - 1, y]) : 0;
-        int dRight = x < WIDTH - 1 ? HeightDifferenceRaw(evalution[x, y], evalution[x + 1, y]) : 0;
-        int dBack = y > 0 ? HeightDifferenceRaw(evalution[x, y], evalution[x, y - 1]) : 0;
-        int dForward = y < HEIGHT - 1 ? HeightDifferenceRaw(evalution[x, y], evalution[x, y + 1]) : 0;
+        Instantiate(surfaceBlockPrefabs[(int)type], new Vector3(x, height - 1, y), Quaternion.identity);
+        yield return null;
+        Instantiate(bedrockPrefab, new Vector3(x, 0, y), Quaternion.identity);
+        GameObject block = blockPrefabs[(int)type];
+        for (int z = 1; z < height - 1; z++)
+        {
+            Instantiate(block, new Vector3(x, z, y), Quaternion.identity);
+            yield return null;
+        }
+    }
+
+    int MaxHeightDifference(int x, int y)
+    {
+        int dLeft = x > 0 ? HeightDifference(evalution[x, y], evalution[x - 1, y]) : 0;
+        int dRight = x < WIDTH - 1 ? HeightDifference(evalution[x, y], evalution[x + 1, y]) : 0;
+        int dBack = y > 0 ? HeightDifference(evalution[x, y], evalution[x, y - 1]) : 0;
+        int dForward = y < HEIGHT - 1 ? HeightDifference(evalution[x, y], evalution[x, y + 1]) : 0;
         return Math.Max(dLeft, Math.Max(dRight, Math.Max(dBack, dForward)));
     }
 
-    int HeightDifferenceRaw(float here, float there)
+    int MinHeightDifference(int x, int y)
+    {
+        int dLeft = x > 0 ? HeightDifference(evalution[x, y], evalution[x - 1, y]) : int.MaxValue;
+        int dRight = x < WIDTH - 1 ? HeightDifference(evalution[x, y], evalution[x + 1, y]) : int.MaxValue;
+        int dBack = y > 0 ? HeightDifference(evalution[x, y], evalution[x, y - 1]) : int.MaxValue;
+        int dForward = y < HEIGHT - 1 ? HeightDifference(evalution[x, y], evalution[x, y + 1]) : int.MaxValue;
+        return Math.Min(dLeft, Math.Min(dRight, Math.Min(dBack, dForward)));
+    }
+
+    int HeightDifference(float here, float there)
     {
         if (Biome(there) == BlockType.WATER) {
             return Mathf.RoundToInt(here * DEPTH);
@@ -109,11 +152,13 @@ public class ProceduralGenerationScript : MonoBehaviour
             {
                 GameObject tree = treePrefabs[UnityEngine.Random.Range(0, treePrefabs.Length)];
                 Instantiate(tree, new Vector3(sample.x, height, sample.y), tree.transform.rotation);
+                blacklist.Add(sample);
             }
             else if (Biome(evalution[(int)sample.x, (int)sample.y]) == BlockType.SAND)
             {
                 GameObject palm = palmPrefabs[UnityEngine.Random.Range(0, palmPrefabs.Length)];
                 Instantiate(palm, new Vector3(sample.x, height, sample.y), palm.transform.rotation);
+                blacklist.Add(sample);
             }
         }
     }
@@ -148,10 +193,15 @@ public class ProceduralGenerationScript : MonoBehaviour
         while (queue.Count > 0)
         {
             Vector3 current = queue.Dequeue();
-            if (Biome(evalution[(int)current.x, (int)current.z]) != BlockType.WATER)
+            if (!blacklist.Contains(new Vector2(current.x, current.z)))
             {
                 playerSpawnPoint = current;
-                playerSpawnPoint.y = DEPTH + 1;
+                playerSpawnPoint.y = Height(evalution[(int)current.x, (int)current.z]);
+                GameObject player = GameObject.FindGameObjectWithTag("Player");
+                player.transform.position = playerSpawnPoint;
+                blacklist.UnionWith(Enumerable.Range(-playerGoalMinimumDistance, playerGoalMinimumDistance * 2 + 1)
+                    .SelectMany(i => Enumerable.Range(-playerGoalMinimumDistance, playerGoalMinimumDistance * 2 + 1)
+                    .Select(j => new Vector2(current.x + i, current.z + j))));
                 return;
             }
             if (current.x > 0) queue.Enqueue(current + Vector3.left);
@@ -159,6 +209,20 @@ public class ProceduralGenerationScript : MonoBehaviour
             if (current.z > 0) queue.Enqueue(current + Vector3.back);
             if (current.z < HEIGHT - 1) queue.Enqueue(current + Vector3.forward);
         }
+    }
+
+    IEnumerator GenerateGoal() {
+        // Select a random point that is not water and is not in the blacklist
+        Vector3 goal;
+        do
+        {
+            Vector2 temp = new(UnityEngine.Random.Range(0, WIDTH), UnityEngine.Random.Range(0, HEIGHT));
+            goal = new Vector3(temp.x, 0, temp.y);
+            blacklist.Add(temp);
+            yield return null;
+        } while (blacklist.Contains(new Vector2(goal.x, goal.z)) && MinHeightDifference((int)goal.x, (int)goal.z) > 1);
+        goal.y = Height(evalution[(int)goal.x, (int)goal.z]);
+        Instantiate(goalPrefab, goal, Quaternion.identity);
     }
 
     BlockType Biome(float e) {
