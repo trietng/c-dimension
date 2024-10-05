4e5c881a1a0edbd0d11983f0c9af1c0614b298bf
bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2024-10-05 21:39:01 +0900

    rearrange main menu UI

---
+++ Assets/Scripts/SettingsPanelScript.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class SettingsPanelScript : MonoBehaviour
+{
+    [SerializeField] public bool inactive = false;
+
+    [SerializeField] public GameObject gameMaster;
+
+    [SerializeField] public Button backButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get back button
+        backButton.onClick.AddListener(() => {
+            if (inactive) return;
+            setInactive();
+            StartCoroutine(backToMenu());
+        });
+    }
+
+    public void setActive () {
+        inactive = false;
+    }
+
+    public void setInactive () {
+        inactive = true;
+    }
+
+    IEnumerator backToMenu () {
+        FadingEffectsScript mainMenuScript = transform.GetComponent<FadingEffectsScript>();
+        mainMenuScript.hide();
+        yield return new WaitUntil(() => !mainMenuScript.visible);
+        gameMaster.GetComponent<UIManagerScript>().mainMenuCanvas.GetComponent<FadingEffectsScript>().show();
+        gameMaster.GetComponent<UIManagerScript>().mainMenuCanvas.GetComponent<MainMenuScript>().inactive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!inactive && Input.GetKeyDown(KeyCode.Escape)) {
+            setInactive();
+            StartCoroutine(backToMenu());
+        }
+    }
+}
